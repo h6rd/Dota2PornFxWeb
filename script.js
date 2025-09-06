240860b8d938e5cc9faf5db5bc35b3b9f5a7aa92
hrdq <51972490+h6rd@users.noreply.github.com>
2025-09-06 21:06:18 +0300

    morph

--- script.js
+++ script.js
@@ -222,7 +222,8 @@ const modsData = {
         { name: 'Bloody Death Prophet', preview: 'Bloody Death Prophet.webp', file: 'Bloody Death Prophet.zip', tags: { effects: true, icons: true }, linkType: 'source', linkUrl: 'https://dota2changer.com/skins_dota_2_mods-death_prophet_custom_modpack/' },
         { name: 'Blue Soul Huskar', preview: 'Blue Soul Huskar.webp', file: 'Blue Soul Huskar.zip', tags: { effects: true, icons: true }, linkType: 'source', linkUrl: 'https://dota2changer.com/skins_dota_2_mods-blue_soul_huskar_megapack/' },
         { name: 'Bloody Lanaya', preview: 'Bloody Lanaya.webp', file: 'Bloody Lanaya.zip', tags: { effects: true, icons: true }, linkType: 'source', linkUrl: 'https://dota2changer.com/skins_dota_2_mods-bloody_lanaya/' },
-        { name: 'Red Abaddon', preview: 'Red Abaddon.webp', file: 'Red Abaddon.zip', tags: { effects: true, icons: true }, linkType: 'source', linkUrl: 'https://dota2changer.com/skins_dota_2_mods-abaddon_red_megapack/' }
+        { name: 'Red Abaddon', preview: 'Red Abaddon.webp', file: 'Red Abaddon.zip', tags: { effects: true, icons: true }, linkType: 'source', linkUrl: 'https://dota2changer.com/skins_dota_2_mods-abaddon_red_megapack/' },
+        { name: 'Void Morphling', preview: 'Void Morphling.webp', file: 'Void Morphling.zip', tags: { effects: true, icons: true }, linkType: 'source', linkUrl: 'https://dota2changer.com/skins_dota_2_mods-void_morphling_by_darknessing/' }
         // { name: '', preview: '.webp', file: '.zip', tags: { effects: true, icons: true }, linkType: 'source', linkUrl: '' },
 
     ],
