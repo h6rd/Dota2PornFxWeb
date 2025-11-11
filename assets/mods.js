e520324f60326d783173437c42af6d37e3b6c735
hrdq <51972490+h6rd@users.noreply.github.com>
2025-11-11 19:11:13 +0300

    Update mods.js

--- assets/mods.js
+++ assets/mods.js
@@ -276,9 +276,9 @@ const modsData = {
     ],
     'tools': [
         { name: 'Background Changer [01.10]', preview: 'Background Changer.webp', file: 'Background Changer.zip', guideId: 'background-changer' },
+        { name: 'Weather Changer [11.11]', preview: 'Weather Changer.webp', file: 'Weather Changer.zip', guideId: 'weather' },
         { name: 'VPKMerge - Combine VPKs', preview: 'VPKMerge.webp', file: 'VPKMerge.zip', guideId: 'vpk-merge' },
         { name: 'VPKTool - Extract & Pack VPKs', preview: 'VPKTool.webp', file: 'VPKTool.zip', guideId: "vpk-tool" },
-        { name: 'Weather Changer [11.11]', preview: 'Weather Changer.webp', file: 'Weather Changer.zip', guideId: 'weather' },
         { name: 'VPKMerge Linux', preview: 'VPKMerge Linux.webp', file: 'VPKMerge Linux.zip', guideId: 'vpk-merge-linux' },
     ],
     'optimization': [
