c37b3f6ca44faa4078bb46562f0a3b0915ecb6aa
hrdq <51972490+h6rd@users.noreply.github.com>
2025-11-20 09:24:48 +0300

    Windranger Autumn Arcana

--- assets/mods.js
+++ assets/mods.js
@@ -1,4 +1,5 @@
 const recentlyAddedMods = [
+    { name: 'Windranger Autumn Arcana', category: 'heroes' },
     { name: 'Primal Beast Snowbeast', category: 'heroes' },
     { name: 'Morphling Megalodon', category: 'heroes' },
     { name: 'Kakehashi Subaru', category: 'backgrounds' },
@@ -303,6 +304,7 @@ const modsData = {
         { name: 'Batrider Night Snotty', preview: 'Batrider Night Snotty.webp', file: 'Batrider Night Snotty.zip', tags: { effects: true, icons: false }, links: [{ type: 'submitted', url: 'https://t.me/hitman47attacks' }, { type: 'source', url: 'https://dota2changer.com' }] },
         { name: 'Morphling Megalodon', preview: 'Morphling Megalodon.webp', file: 'Morphling Megalodon.zip', tags: { effects: true, icons: true }, links: [{ type: 'author', url: 'https://vk.com/defiree2mods' }] },
         { name: 'Primal Beast Snowbeast', preview: 'Primal Beast Snowbeast.webp', file: 'Primal Beast Snowbeast.zip', tags: { effects: false, icons: false }, links: [{ type: 'author', url: 'https://vk.com/id363951132' }] },
+        { name: 'Windranger Autumn Arcana', preview: 'Windranger Autumn Arcana.webp', file: 'Windranger Autumn Arcana.zip', tags: { effects: true, icons: false }, links: [{ type: 'author', url: 'https://vk.com/defiree2mods' }] },
         // { name: '', preview: '.webp', file: '.zip', tags: { effects: true, icons: true }, links: [{ type: 'author', url: '' }] },
     ],
     'roshan': [
