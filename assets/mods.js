dca59f7669f971317427a1cf34bb4db578a06fbe
hrdq <51972490+h6rd@users.noreply.github.com>
2025-11-26 00:54:25 +0300

    Spectre Phantom Advent

--- assets/mods.js
+++ assets/mods.js
@@ -1,4 +1,5 @@
 const recentlyAddedMods = [
+    { name: 'Spectre Phantom Advent', category: 'heroes' },
     { name: 'Phantom Assassin Skull Arcana', category: 'heroes' },
     { name: 'Background Changer', category: 'tools' },
     { name: 'TI9 Ranged Attack', category: 'ranged-attack' },
@@ -334,6 +335,7 @@ const modsData = {
         { name: 'Tinker Deathsman', preview: 'Tinker Deathsman.webp', file: 'Tinker Deathsman.zip', tags: { effects: false, icons: false }, links: [{ type: 'source', url: 'https://dota2changer.com' }] },
         { name: 'Night Stalker Feasts of Forever', preview: 'Night Stalker Feasts of Forever.webp', file: 'Night Stalker Feasts of Forever.zip', tags: { effects: false, icons: false }, links: [{ type: 'source', url: 'https://dota2changer.com' }] },
         { name: 'Phantom Assassin Skull Arcana', preview: 'Phantom Assassin Skull Arcana.webp', file: 'Phantom Assassin Skull Arcana.zip', tags: { effects: true, icons: false }, links: [{ type: 'author', url: 'https://vk.com/defiree2mods' }] },
+        { name: 'Spectre Phantom Advent', preview: 'Spectre Phantom Advent.webp', file: 'Spectre Phantom Advent.zip', tags: { effects: true, icons: true }, links: [{ type: 'author', url: 'https://vk.com/id363951132' }] },
         // { name: '', preview: '.webp', file: '.zip', tags: { effects: true, icons: true }, links: [{ type: 'author', url: '' }] },
     ],
     'roshan': [
