21977b24a78642aa4785ccf35037fc5d6368f4ee
hrdq <51972490+h6rd@users.noreply.github.com>
2025-12-31 16:58:37 +0300

    Rubick

--- assets/mods.js
+++ assets/mods.js
@@ -1,5 +1,6 @@
 const recentlyAddedMods = [
     // { name: '', category: '' },
+    { name: 'Rubick Plagueroad Apothacary', category: 'heroes' },
     { name: 'Aghanim Red Lite', category: 'shaders' },
     { name: 'Aghanim Yellow Lite', category: 'shaders' },
     { name: 'Aghanim White Lite', category: 'shaders' },
@@ -598,6 +599,7 @@ const modsData = {
         { name: 'Nude Drow Ranger', preview: 'Nude Drow Ranger.webp', file: 'Nude Drow Ranger.zip', tags: { effects: false, icons: false }, links: [{ type: 'author', url: 'https://discord.com/users/187454472253997056' }] },
         { name: 'Nude Queen of Pain', preview: 'Nude Queen of Pain.webp', file: 'Nude Queen of Pain.zip', tags: { effects: false, icons: false }, links: [{ type: 'source', url: 'https://ru.dota2changer.com/skins_dota_2_mods-royal_decree_nsfw_edition/' }] },
         { name: 'Shadow Fiend Trenbolone', preview: 'Shadow Fiend Trenbolone.webp', file: 'Shadow Fiend Trenbolone.zip', tags: { effects: true, icons: false }, links: [{ type: 'author', url: 'https://steamcommunity.com/id/hooorde' }] },
+        { name: 'Rubick Plagueroad Apothacary', preview: 'Rubick Plagueroad Apothacary.webp', file: 'Rubick Plagueroad Apothacary.zip', tags: { effects: true, icons: false }},
         // { name: '', preview: '.webp', file: '.zip', tags: { effects: true, icons: true }, links: [{ type: 'author', url: '' }] },
     ],
     'roshan': [
