0c834356cf11806667bd1ee21d57f879d835f567
hrdq <51972490+h6rd@users.noreply.github.com>
2026-07-22 11:41:51 +0300

    fck CORS

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 const FILES_BASE_URL = (() => {
     const host = window.location.hostname;
-
-    const codebergRawHosts = [
-        'hrdq.codeberg.page',
-    ];
-    const codebergRawSuffix = '.pages.dev';
-
-    if (
-        codebergRawHosts.some(h => host === h || host.endsWith('.' + h)) ||
-        host.endsWith(codebergRawSuffix)
-    ) {
+    if (host === 'hrdq.codeberg.page' || host.endsWith('.hrdq.codeberg.page')) {
         return 'https://codeberg.org/hrdq/Dota2PornFxWeb/raw/branch/main';
     }
 
@@ -21,7 +12,7 @@ const FILES_BASE_URL = (() => {
     ];
 
     if (localHosts.some(h => host === h || host.endsWith('.' + h))) {
-        return '';
+        return ''; 
     }
 
     return 'https://raw.githubusercontent.com/h6rd/Dota2PornFxWeb/main';
