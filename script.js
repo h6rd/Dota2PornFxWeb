569521a9ee04b15e18977d74a7b8049818da76be
hrdq <51972490+h6rd@users.noreply.github.com>
2026-08-26 10:43:08 +0300

    mods: heroes

--- script.js
+++ script.js
@@ -2616,7 +2616,7 @@ function generateTagsHtml(mod, categoryId) {
     const activeTags = [];
     const tagMap = cfg.map || {};
 
-    const SLOT_TAGS = ['totem', 'weapon', 'mount', 'head', 'arm', 'arms', 'armor', 'shoulders', 'back', 'shield', 'hair', 'neck', 'rocket'];
+    const SLOT_TAGS = ['base', 'totem', 'weapon', 'mount', 'head', 'arm', 'arms', 'armor', 'shoulders', 'back', 'shield', 'hair', 'neck', 'rocket'];
 
     for (const key in tagMap) {
         if (Object.prototype.hasOwnProperty.call(mod.tags, key) && mod.tags[key]) {
